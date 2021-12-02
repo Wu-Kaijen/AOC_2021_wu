7fafc03f222555668a44782f2bc7411b7d0118df
Wu Kaijen <75332096+Wu-Kaijen@users.noreply.github.com>
2021-12-02 18:15:54 +0100

    Add files via upload

--- Day1/Day 1 .py
+++ Day1/Day 1 .py
@@ -1,20 +1,21 @@
 import numpy as np
 import csv
 
-with open('Data.csv', newline= '') as file: 
-    reader = csv.reader(file, quoting = csv.QUOTE_NONNUMERIC, delimiter =  ' ')
-    output  = []
-    for row in reader:
-        output.append(row[:])
+""" input data  """
+output = [ int(i[0]) for i in csv.reader(open('Data_test.csv'))]
 
-
+""" part 1 """
 n = 0 
 for i in range(1,len(output)):
-    if output[i][0] > output[i-1][0]:
+    if output[i] > output[i-1]:
         n += 1
 
-
+""" part 2 """
 N = 0
-for i in range(0,len(output)-2):
-    if output[i+3][0] > output[i][0]:
+for i in range(0,len(output)-3):
+    if output[i+3] > output[i]:
         N += 1
+
+""" output """
+print(f'number of increases {n}')
+print(f'number of sliding increases {N}')
